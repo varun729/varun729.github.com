ad4be1a3d92a26af42ab72d2c0b2020f3ed01600
Varun Agrawal <varun729@gmail.com>
2012-01-14 02:26:39 +0530

    on pressing new line, the pre content gets loaded

--- new/js/terminal.js
+++ new/js/terminal.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
-	var content = null;
+        function initialize(initial, options) {
+                $('.terminal').val(initial);
+        }
+	var pre = "[guest@agrawal-varun.com]$ ";
+        var cursor = 0;
+        var content = null;
 
-	$('.terminal').keydown(function(e) {
+        var pre_size = pre.length;
+
+	initialize(pre);
+
+        function new_line_event() {
+                var old_val = $('.terminal').val();
+                $('.terminal').val(old_val + '\n' + pre);
+        }
+        
+        $('.terminal').keydown(function(e) {
 		var input = e.which === undefined ? e.keyCode : e.which;
-		if (input != 13 || e.shiftKey)
-			return;
-		content = $(this).val();
+                cursor = $(this).getSelection();
+                // arrow keys disabled
+		if (input == 38 || input == 40 || input == 37 || input == 39) 
+                        e.preventDefault();
+                // not an enter key
+                if (input != 13 || e.shiftKey) {
+                        return;
+                }
+                e.preventDefault();
+		var new_content = $(this).val();
+		new_line_event();
 	});
 });
