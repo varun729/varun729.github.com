edf5449ca71fd16f80cd431e10653a79d6f89dab
Varun Agrawal <varun729@gmail.com>
2012-01-14 05:31:53 +0530

    buffer implemented for past and future commands entered when pressed up or down arrow

--- new/js/terminal.js
+++ new/js/terminal.js
@@ -7,31 +7,33 @@ $(document).ready(function() {
                 term.focus();
                 term.val('');   // TODO Looks like a hack
                 term.val(initial);
-        }
-	var pre = "[guest@agrawal-varun.com]$ ";
+        };
+        var pre = "[guest@agrawal-varun.com]$ ";
         var pre_size = pre.length;
         
         var content = null;
         var buffer = Array("");
+        var buffer_index = -1;
 
-	initialize(pre);
+        initialize(pre);
 
         /**
          * When the enter key is pressed. the events to be fired
          */
         function new_line_event() {
                 $('.terminal').val(content + '\n' + pre);
-        }
+        };
 
         /**
          * get the last command entered
          */
         function last_line(old_lines, lines, prefix) {
                 if (old_lines != null) {
-                        lines = lines.substring(old_lines.length);
+                        lines = lines.substring(old_lines.length + 1);
+                                        // +1 because of new iine
                 }
                 return lines.substring(pre_size);
-        }
+        };
 
         /**
          * Get the current typed command, till the last key pressed.
@@ -41,7 +43,7 @@ $(document).ready(function() {
                 var old_content = content;
                 var new_content = $('.terminal').val();
                 return last_line(old_content, new_content, pre);
-        }
+        };
 
         /**
          * load the buffer with the command
@@ -51,12 +53,15 @@ $(document).ready(function() {
         function load_buffer(dummy) {
                 if (dummy) {
                         buffer.push("");
+                        if (buffer_index == -1) {
+                                buffer_index = buffer.length - 1;
+                        }
                         return;
                 }
                 var last = typed();
                 var buff_last = buffer.pop();
                 buffer.push(last);
-        }
+        };
 
         /**
          * position of the cursor for the current command
@@ -66,7 +71,7 @@ $(document).ready(function() {
                                         // +1 is for new line character
                 var cursor_pos = $('.terminal').getSelection().end;
                 return cursor_pos - past_size;
-        }
+        };
 
         /**
          * check the cursor position for the current command being typed
@@ -75,31 +80,81 @@ $(document).ready(function() {
                 if (cursor > pre_size)
                         return true;
                 return false;
-        }
+        };
+
+        /**
+         * reset the buffer index
+         */
+        function reset_buffer() {
+                buffer_index = -1;
+        };
+
+        /**
+         * decrement the buffer index by 1, if possible
+         */
+        function decrement_buf_index() {
+                if (buffer_index > 0) {
+                        buffer_index -= 1;
+                }
+        };
+
+        /**
+         * increment the buffer index by 1, if possible
+         */
+        function increment_buf_index() {
+                if (buffer_index < buffer.length - 1) {
+                        buffer_index += 1;
+                }
+        };
+
+        /**
+         * get the buffer element at the buffer_index
+         */
+        function get_buffer() {
+                return buffer[buffer_index];
+        };
+
+        /**
+         * update the current command from the buffer
+         */
+        function update_command(com) {
+                var new_content = content + "\n" + pre + com;
+                $('.terminal').val('');
+                $('.terminal').val(new_content);
+        };
 
         /**
          * run the command
          */
         function run_command(command) {
 
-        }
+        };
         
         $('.terminal').keydown(function(e) {
-		var input = e.which === undefined ? e.keyCode : e.which;
+                var input = e.which === undefined ? e.keyCode : e.which;
 
-                // TODO backspace and left arrow disabled if the cursor is to the extreme left
+                // backspace and left arrow disabled if the cursor 
+                // is to the extreme left
                 if (input == 8 || input == 37) {
                         var cursor = cursor_position();
                         if (!is_valid(cursor)) {
-                                // TODO check if the cursor is at a valid position, otherwise
-                                // put it to the beginning of the command line
                                 e.preventDefault();
                         }
                 }
-                // up-down arrow keys disabled
-		if (input == 38 || input == 40) {
-                        // TODO use buffer to load the past and present commands
+                // up-down arrow keys default action disabled
+                if (input == 38 || input == 40) {
                         e.preventDefault();
+                        if (buffer_index == -1) {
+                                return;
+                        }
+                        if (input == 38) { // up key
+                                decrement_buf_index();
+                        } else {
+                                increment_buf_index();
+                        }
+                        var com = get_buffer();
+                        update_command(com);
+                        return;
                 }
                 // not an enter key
                 if (input != 13 || e.shiftKey) {
@@ -113,9 +168,10 @@ $(document).ready(function() {
                 content = $(this).val();
 
                 run_command(last);
-		load_buffer(true);
+                reset_buffer();
+                load_buffer(true);
                 new_line_event();
-	});
+        });
         
         /**
          * Disabling default mouse actions on the terminal.
