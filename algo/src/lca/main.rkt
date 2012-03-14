39bd806b5e7bc16f0b69e7f9655dd79d98f8976d
Varun Agrawal <varun@agrawal-varun.com>
2012-03-14 23:13:58 +0530

    adding gif images to the homepage

--- algo/src/lca/main.rkt
+++ algo/src/lca/main.rkt
@@ -1,5 +1,7 @@
 #lang racket
 (require racket/include)
+(require racket/gui)
+(require mrlib/gif)
 (require "tree.rkt")
 (require "draw_tree.rkt")
 
@@ -12,16 +14,44 @@
       (draw-tree tree units show-value)
       (save-tree tree units show-value f)))
 
+;;; file name
+(define (get-file-name s type)
+  (string-append "../../img/tree/" 
+                 (if (number? s)
+                     (number->string s)
+                     s)
+                 "." type))
+
+;;; gif
+(define (get-tree-bitmap img)
+  (make-object bitmap% img 'png))
+
+(define (create-tree-gif delay name img-list)
+  (define tree-bitmap-list (map get-tree-bitmap img-list))
+  (write-animated-gif tree-bitmap-list delay (get-file-name name "gif") #:loop? true #:one-at-a-time? false #:last-frame-delay 100))
+
+
+;;; user-controlled part
+(define (draw-tree-list-gif name tree-list)
+  (define (show-tree-fu lis n)
+    (cond ((null? lis) null)
+          (else (show-tree (car lis) (get-file-name n "png"))
+                (show-tree-fu (cdr lis) (+ n 1)))))
+  (show-tree-fu tree-list 1)
+  (create-tree-gif 100 name (map (lambda (y) (get-file-name y "png"))
+                                 (build-list 
+                                  (length tree-list)
+                                  (lambda (y)
+                                    (+ 1 (values y)))))))
+
 
 
 ;;; example
-;(show-tree '(1(2)(3(4(10)(* "red" 11))(5(6)(* "red" 7)(8)(9)))) null)
+(show-tree '(1(2)(3(4(10)(11))(5(6)(7)(8)(9)))) (get-file-name "tree" "png"))
 
 ;;; linear tree
-(show-tree '(1(2(3(4(5(6(7))))))) "../../img/linear/1.png")
-(show-tree '(1(2(3(* "red" 4(5(* "red" 6(7))))))) "../../img/linear/2.png")
-(show-tree '(1(2(* "blue" 3(* "red" 4(5(* "red" 6(7))))))) "../../img/linear/3.png")
-;(show-tree '(1(2(3(4(5(6(7))))))) "../../img/linear/4.png")
-;(show-tree '(1(2(3(4(5(6(7))))))) "../../img/linear/5.png")
-;(show-tree '(1(2(3(4(5(6(7))))))) "../../img/linear/6.png")
-
+(draw-tree-list-gif "linear" 
+                    (list
+                     '(1(2(3(4(5(6(7)))))))
+                     '(1(2(3(* "blue" 4(5(* "blue" 6(7)))))))
+                     '(1(2(* "red" 3(* "blue" 4(5(* "blue" 6(7)))))))))
