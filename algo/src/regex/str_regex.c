41304d2ab4695a90aca7b8ff93587cf6f9b3c803
Varun Agrawal <varun729@gmail.com>
2012-06-13 18:31:46 +0530

    add the compilation code in gcc. Need to write it for clang

--- algo/src/regex/str_regex.c
+++ algo/src/regex/str_regex.c
@@ -1,7 +1,24 @@
 /**
  * regex library
  */
-#include <stdio.h>
+#include <stdlib.h>
 #include "str_regex.h"
 
+struct reg_exp create_regexp(char *pattern)
+{
+	struct reg_exp *exp;
+	exp = malloc(sizeof(struct reg_exp));
+	exp->pattern = pattern;
+	return *exp;
+}
 
+int reg_match(struct reg_exp *expression, char *str)
+{
+	// TODO
+	return 0;
+}
+
+void reg_replace(struct reg_exp *expression, char *str, char *rstr)
+{
+	// TODO
+}
--- algo/src/regex/str_regex.h
+++ algo/src/regex/str_regex.h
@@ -10,22 +10,22 @@ struct reg_exp {
 /**
  * create regular expression
  * pattern : regular expression pattern
- * return regular expression object
+ * return pointer to regular expression object
  */
 struct reg_exp create_regexp(char *pattern);
 
 /**
  * check if the regular expression is found in a string
- * expression : regular expression
+ * expression : pointer to regular expression
  * str : string
  * return location
  */
-int reg_match(struct reg_exp expression, char *str);
+int reg_match(struct reg_exp *expression, char *str);
 
 /**
  * check if the regular expression is found, if present replace it
- * expression : regular expression
+ * expression : pointer to regular expression
  * str : string to be searched
  * rstr : replacement string
  */
-void reg_replace(struct reg_exp expression, char *str, char *rstr);
+void reg_replace(struct reg_exp *expression, char *str, char *rstr);
